3c0aa3302149d7e9a923eb35d9b574825dfaaeea
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-09-26 10:17:27 +0600

    setup All file

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => '51dbcb449eeb8ab8c663');
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const {registerBlockType} = wp.blocks;
+
+registerBlockType('myguten/test-block', {
+    title: 'Basic Example',
+    icon: 'smiley',
+    category: 'layout',
+    edit: ({className}) => <div className={className}>Hello World</div>,
+    save: () => <div>Hello World</div>,
+
+});
